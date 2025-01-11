f48ef2a411c116db7e1629feffd063c996f7903d
huyb-cyber <huxubin4@gmail.com>
2025-01-11 17:21:35 +0800

    compleed goods_list

---
+++ src/components/my-goods/my-goods.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="goods-item">
+    <div class="goods-item-left">
+      <image class="goods-pic" :src="goods.goods_small_logo || defaultPic">
+      </image>
+    </div>
+    <div class="goods-item-right">
+      <div class="goods-name">{{ goods.goods_name }}</div>
+      <div class="goods-info-box">
+        <div class="goods-price">￥{{ goods.goods_price | toFixed }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-goods",
+  props: {
+    goods: {
+      type: Object,
+      default: {},
+    },
+  },
+  data() {
+    return {
+      defaultPic:
+        "https://c-ssl.dtstatic.com/uploads/blog/202407/30/q4S8L8J5SxoXxZL.thumb.1000_0.jpg",
+    };
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  filters: {
+    toFixed(num) {
+      return Number(num).toFixed(2);
+    },
+  },
+
+  // 组件周期函数--监听组件挂载完毕
+  mounted() {},
+  // 组件周期函数--监听组件数据更新之前
+  beforeUpdate() {},
+  // 组件周期函数--监听组件数据更新之后
+  updated() {},
+  // 组件周期函数--监听组件激活(显示)
+  activated() {},
+  // 组件周期函数--监听组件停用(隐藏)
+  deactivated() {},
+  // 组件周期函数--监听组件销毁之前
+  beforeDestroy() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.goods-item {
+  display: flex;
+  padding: 20rpx 10rpx;
+  border-bottom: 2rpx solid #f0f0f0;
+
+  .goods-item-left {
+    margin-right: 10rpx;
+
+    .goods-pic {
+      width: 200rpx;
+      height: 200rpx;
+      display: block;
+    }
+  }
+
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+
+    .goods-name {
+      font-size: 26rpx;
+    }
+
+    .goods-price {
+      font-size: 32rpx;
+      color: #c00000;
+      text-align: right;
+    }
+  }
+}
+</style>
--- src/subpkg/goods_list/index.vue
+++ src/subpkg/goods_list/index.vue
@@ -1,19 +1,72 @@
 <template>
-  <div class="goods_list">goods_list</div>
+  <div class="goods-list">
+    <view v-for="(goods, i) in goodsList" :key="i" @click="toDetail(goods)">
+      <my-goods :goods="goods" />
+    </view>
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return {};
+    return {
+      queryObj: {
+        query: "",
+        cId: "",
+        pageNum: 1,
+        pageSize: 10,
+      },
+      goodsList: [],
+      total: 0,
+      isLoading: false,
+    };
   },
   computed: {},
-  methods: {},
+  methods: {
+    toDetail(good) {
+      uni.navigateTo({
+        url: "/subpkg/goods_detail/goods_detail?goods_id=" + good.goods_id,
+      });
+    },
+    async getGoodsList(cb) {
+      this.isLoading = true;
+
+      const { data: res } = await uni.$http.get(
+        "/api/public/v1/goods/search",
+        this.queryObj
+      );
+
+      this.isLoading = false;
+
+      cb && cb();
+      if (res.meta.status !== 200) {
+        return uni.$showMsg();
+      }
+
+      this.goodsList = [...this.goodsList, ...res.message.goods];
+      this.total = res.message.total;
+    },
+  },
   watch: {},
+  onReachBottom() {
+    if (this.queryObj.pageNum * this.queryObj.pageSize >= this.total) {
+      return uni.$$showMsg("数据加载完毕");
+    }
+    if (this.isLoading) {
+      return;
+    }
+
+    this.queryObj.pageNum++;
+    this.getGoodsList();
+  },
 
   // 页面周期函数--监听页面加载
-  onLoad() {},
+  onLoad(options) {
+    this.queryObj.query = options.query || "";
+    this.queryObj.cId = options.cid || "";
+    this.getGoodsList();
+  },
   // 页面周期函数--监听页面初次渲染完成
   onReady() {},
   // 页面周期函数--监听页面显示(not-nvue)
@@ -23,7 +76,13 @@ export default {
   // 页面周期函数--监听页面卸载
   onUnload() {},
   // 页面处理函数--监听用户下拉动作
-  // onPullDownRefresh() { uni.stopPullDownRefresh(); },
+  onPullDownRefresh() {
+    this.queryObj.pageNum = 1;
+    this.total = 0;
+    this.isLoading = false;
+    this.goodsList = [];
+    this.getGoodsList(() => uni.stopPullDownRefresh());
+  },
   // 页面处理函数--监听用户上拉触底
   // onReachBottom() {},
   // 页面处理函数--监听页面滚动(not-nvue)
@@ -33,4 +92,5 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
